6d16052f1137d186893ced1ea7c6a4faf1b34457
Your Name <you@example.com>
2019-03-15 15:20:03 +0000

    Template

--- src/components/Bootcampers/index.js
+++ src/components/Bootcampers/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Bootcampers = () => {
-  return <div className="" />;
+  return (
+    <div className="">
+      <div className="leftTopBox" />
+      <div className="rightTopBox" />
+    </div>
+  );
 };
 
 export default Bootcampers;
--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Timer = () => {
-  return <div className="" />;
+  return <div className="timerBox" />;
 };
 
 export default Timer;
