4babb54202200546ba5e5e345dd90c8ad4ea294e
karenchahal <karenjeet.chahal@gmail.com>
2019-03-15 14:18:32 +0000

    addition of bootcampers

---
+++ src/data/bootcampers.js
@@ -0,0 +1,94 @@
+export default [
+  {
+    id: 1,
+    name: "Aidan"
+  },
+  {
+    id: 2,
+    name: "Alex"
+  },
+  {
+    id: 3,
+    name: "Ben"
+  },
+  {
+    id: 4,
+    name: "Brendan"
+  },
+  {
+    id: 5,
+    name: "Dave"
+  },
+  {
+    id: 6,
+    name: "Jazz"
+  },
+  {
+    id: 7,
+    name: "Johnny"
+  },
+  {
+    id: 8,
+    name: "Karen"
+  },
+  {
+    id: 9,
+    name: "Kathryn"
+  },
+  {
+    id: 10,
+    name: "Kim"
+  },
+  {
+    id: 11,
+    name: "Kira"
+  },
+  {
+    id: 12,
+    name: "Liam"
+  },
+  {
+    id: 13,
+    name: "Manu"
+  },
+  {
+    id: 14,
+    name: "Mareen"
+  },
+  {
+    id: 15,
+    name: "Matt"
+  },
+  {
+    id: 16,
+    name: "Olivia"
+  },
+  {
+    id: 17,
+    name: "Omar"
+  },
+  {
+    id: 18,
+    name: "Rhys"
+  },
+  {
+    id: 19,
+    name: "Robyn"
+  },
+  {
+    id: 20,
+    name: "Sahela"
+  },
+  {
+    id: 21,
+    name: "Stuart"
+  },
+  {
+    id: 22,
+    name: "Wasim"
+  },
+  {
+    id: 23,
+    name: "Bukola"
+  }
+];
