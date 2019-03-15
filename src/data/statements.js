077177bf8ec026c34db310d839afc7cd71fbfc8c
karenchahal <karenjeet.chahal@gmail.com>
2019-03-15 14:16:47 +0000

    addition of statements and data folder

---
+++ src/data/statements.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    statement: "Kim Kardashian vs The Rock for president"
+  },
+  {
+    statement: "Going out vs Nextflix & Chill"
+  },
+  {
+    statement: "Episodes 1-3 Star Wars vs Episodes 4-6 Star Wars"
+  },
+
+  {
+    statement: "Beauty vs Brains"
+  },
+
+  {
+    statement: "Xbox vs Playstation"
+  },
+  {
+    statement: "Should Santa's elves be paid minimum wage?"
+  },
+  {
+    statement: "Socks with sandals - yay or nay"
+  },
+  {
+    statement: "The Office UK vs The Office US"
+  },
+  {
+    statement: " Are magic 8 balls more reliable than tarot cards?"
+  },
+  {
+    statement: "Liverpool should win the Premier League 2019"
+  },
+  {
+    statement: "Should pineapple be allowed to be put on pizza?"
+  },
+  {
+    statement: "Is Kanye West a genius?"
+  },
+  {
+    statement: "Were Ross and Rachel really on a break?"
+  },
+  {
+    statement: "Is it okay to pee whilst taking a shower?"
+  }
+];
